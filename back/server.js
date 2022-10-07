d50d778b7ca4cc43534af8b212ed23c1369f04bf
fortegaj <f.r.o.j22@hotmail.com>
2022-10-07 06:35:17 -0300

    Creación del server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,12 @@
-const app= require("app")
+//Sirve para arrancar el servidor 
+
+const app= require("./app")
+
+//Setear el archivo de configuración / Importar la libreria config.env
+const dotenv= require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+//Construimos un método para que me verifique que estoy conectado al servidor
+const server= app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
